ae41da885a8bca24926f14aa91abbf7ead038bb8
2000031617 <2000031617@kluniversity.in>
2022-11-09 01:13:42 +0530

    make it better

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+let port = process.env.port || 8080;
+
+App.listen(port, ()=>{
+  // eslint-disable-next-line no-template-curly-in-string
+  console.log('App is running at port ${port}');
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
